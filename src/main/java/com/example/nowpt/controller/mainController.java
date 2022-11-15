b8a4ca3cf2864082e977cc9bea0a6ae5875cb3f9
jiwon.kim <jiwon.kim01@e4net.net>
2022-11-15 11:19:53 +0900

    thymeleaf add

--- src/main/java/com/example/nowpt/controller/mainController.java
+++ src/main/java/com/example/nowpt/controller/mainController.java
@@ -1,33 +1,32 @@
 package com.example.nowpt.controller;
 
 import com.example.nowpt.model.Member;
-import com.example.nowpt.model.QMember;
-import com.example.nowpt.model.Test2;
 import com.example.nowpt.service.TestService;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 
-@RestController
+@Controller
 @Slf4j
 public class mainController {
 
     @Autowired private TestService testService;
 
     @RequestMapping({"/main","/"})
-    public String main(){
+    public String main(Model model){
 
-        log.debug("jwCheck{}",testService.selectAllTest());
 
         List<Member> lists = testService.selectAllTest();
+        log.debug("jwCheck{}",testService.selectAllTest());
 
-        for(Member data: lists){
-            System.out.println(data);
-        }
-        return lists.toString();
+        model.addAttribute("lists",lists);
+
+        return "mvc/main";
     }
 
 
