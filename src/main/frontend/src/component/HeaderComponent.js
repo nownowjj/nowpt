ec007cd7704de6e320b502d2c28f6f68113a7d4c
jiwon.kim <jiwon.kim01@e4net.net>
2022-11-21 10:05:12 +0900

    BE - FE LINE13

--- src/main/frontend/src/component/HeaderComponent.js
+++ src/main/frontend/src/component/HeaderComponent.js
@@ -12,8 +12,8 @@ function HeaderComponent (){
             <Container>
                 <Navbar.Brand href="#home">Navbar</Navbar.Brand>
                 <Nav className="me-auto">
-                    <Nav.Link as={NavLink} to='/home'>Home</Nav.Link>
-                    <Nav.Link as={NavLink} to='/test'>test</Nav.Link>
+                    <Nav.Link as={NavLink} to='/api'>Home</Nav.Link>
+                    <Nav.Link as={NavLink} to='/api/test'>test</Nav.Link>
                     {/*<Nav.Link href="#pricing">Pricing</Nav.Link>*/}
                 </Nav>
             </Container>
