929b67bbef84deebc9ea6f5be02baef4b7318e83
jiwon.kim <jiwon.kim01@e4net.net>
2022-12-28 16:50:07 +0900

    로그인 유효성 검사

---
+++ src/main/frontend/src/services/validate.js
@@ -0,0 +1,42 @@
+/**
+ * 
+ * @param email 
+ * @param originEmail
+ * @returns {boolean}
+ * 이메일 변경 유효성 검사
+ */
+export function validateEmail(email,originEmail){
+    console.log("검증 실행")
+    if(email.length < 5){
+        alert("값이 부족해");
+        console.log("1 : 통과 못함");
+        return false;
+    }
+    if( (email.includes('@')) === false ){
+        alert("@ 없어요");
+        console.log("2 : 통과 못함");
+        return false;
+    }
+    if(email === originEmail){
+        alert("값이 똑같아요");
+        return false;
+    }
+    console.log("다 통과함");
+    return true;
+}
+
+/**
+ * @author jiwon.kim
+ * @param id
+ * @param pw
+ * @returns {boolean}
+ * 로그인 유효성 검사
+ */
+export function validateLogin(id,pw){
+    console.log("validate")
+    if(id === "" || pw === ""){
+        alert("Id 또는 PassWord를 입력하지 않았습니다. ")
+        return false;
+    }
+    return true;
+}
