3cc20eb83b1ad73113c8f26504eba2d6d450d428
jiwon.kim <jiwon.kim01@e4net.net>
2023-01-03 11:39:25 +0900

    Paging default 값 주석처리

--- src/main/java/com/example/nowpt/NowptApplication.java
+++ src/main/java/com/example/nowpt/NowptApplication.java
@@ -2,6 +2,8 @@ package com.example.nowpt;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.data.web.config.PageableHandlerMethodArgumentResolverCustomizer;
 
 @SpringBootApplication
 public class NowptApplication {
@@ -9,5 +11,13 @@ public class NowptApplication {
     public static void main(String[] args) {
         SpringApplication.run(NowptApplication.class, args);
     }
+//    @Bean
+//    public PageableHandlerMethodArgumentResolverCustomizer customize() {
+//        return p -> {
+//            p.setOneIndexedParameters(true);	// 1부터 시작
+//            p.setMaxPageSize(10);				// size=10
+//        };
+//    }
+
 
 }
