90b6036208653630b80d71550741419a789da8ec
jiwon.kim <jiwon.kim01@e4net.net>
2023-01-12 14:38:28 +0900

    예약 리스트 페이징 처리 및 한글처리

--- src/main/frontend/src/pages/meetingRoom/MeetingListComponent.js
+++ src/main/frontend/src/pages/meetingRoom/MeetingListComponent.js
@@ -1,14 +1,21 @@
-import React, {useEffect} from 'react';
+import React, {useEffect, useState} from 'react';
 import styled from "styled-components";
-import {useState} from "react";
-import DatePicker from "react-datepicker";
+import DatePicker, {registerLocale} from "react-datepicker";
 import "react-datepicker/dist/react-datepicker.css"
-import {getNotice} from "../../api/NoticeApi";
 import {getReservation} from "../../api/ReservationApi";
 import dayjs from "dayjs";
+import 'dayjs/locale/ko';
+// import relativeTime from 'dayjs/plugin/relativeTime';
+import ko from "date-fns/locale/ko";
+import roomData from "../../db/MeetingRoomData.json";
+import isId from "../../services/isId";
 import Button from "../../component/JoinButton";
+import {useNavigate} from "react-router-dom";
 
 const MeetingListComponent = ({data}) => {
+    const navigate = useNavigate();
+    dayjs.locale('ko');
+    registerLocale("ko", ko);
     // 일주일 리미트 셋
     // 검색 시작날짜인데 일자를 크게 두면 DB에 무리가 갈수 있으므로
     // 현재 날짜에서 일주일을 뺀 날짜로 startDate를 set해준다
@@ -23,6 +30,10 @@ const MeetingListComponent = ({data}) => {
     // 예약 리스트
     const [reservationList,setReservationList] = useState([]);
 
+    const [room , setRoom] = useState("대회의실");
+
+    const [id] = useState(isId())
+
     const setStartDateEvent=(date)=>{
         setStartDate(date);
     }
@@ -31,9 +42,33 @@ const MeetingListComponent = ({data}) => {
         setEndDate(date);
     }
 
+    const handleRoomChange = (e) => {
+        setRoom(e.target.value);
+        console.log(e.target.value);
+    };
+
+    const reservationUpdate = reservationSn =>{
+        console.log("클릭 이벤트" + reservationSn);
+        navigate("/go/meetingRoom/"+reservationSn);
+    }
+
+
     useEffect(()=>{
-        console.log("페이지 컴포넌트 data : " ,data)
-        getReservation()
+
+        // 예약이 새롭게 등록되면 (등록 컴포넌트) > 페이지 > (리스트 컴포넌트)로 증가된 data = 0..1..2가 들어옴
+        // console.log("페이지 컴포넌트 data : " ,data)
+
+        let paramStart =(startDate.getFullYear() + '-' + (startDate.getMonth() + 1).toString().padStart(2, '0') + '-' + startDate.getDate().toString().padStart(2, '0'));
+        let paramEnd =(endDate.getFullYear() + '-' + (endDate.getMonth() + 1).toString().padStart(2, '0') + '-' + endDate.getDate().toString().padStart(2, '0'));
+
+        let paramDto = {
+            "startDay":paramStart,
+            "endDay":paramEnd,
+            "room":room
+        }
+        console.log("페이지 파라미터 %O",paramDto);
+
+        getReservation(paramStart,paramEnd,room)
             .then(response => {
                 console.log(response);
                 setReservationList(response.content);
@@ -47,24 +82,34 @@ const MeetingListComponent = ({data}) => {
             <h2>예약 현황</h2>
 
             <MyDatePicker
+                locale="ko"
                 selected={startDate}
-                // value='년도-월-일 선택'
                 onChange={(date) => setStartDateEvent(date)}
                 selectsStart
                 startDate={startDate}
                 endDate={endDate}
-                dateFormat="yyyy년 MM월 dd일"
+                dateFormat="yyyy년 MM월 dd일(eee)"
                 maxDate={endDate}
             />
             <MyDatePicker
+                locale="ko"
                 selected={endDate}
                 onChange={(date) => setEndDateEvent(date)}
                 selectsEnd
                 endDate={endDate}
                 minDate={startDate}
-                dateFormat="yyyy년 MM월 dd일"
+                dateFormat="yyyy년 MM월 dd일(eee)"
             />
 
+            <select onChange={handleRoomChange}>
+                {roomData.meetingRoom.map((room) => (
+                    <option key={room.id}
+                            value={room.value}
+                    >
+                        {room.roomName}
+                    </option>
+                ))}
+            </select>
 
             <table>
                 <thead>
@@ -78,6 +123,7 @@ const MeetingListComponent = ({data}) => {
                     <th>수정 시간</th>
                     <th>등록자</th>
                     <th>사용목적</th>
+                    <th>수정</th>
                 </tr>
                 </thead>
                 <tbody>
@@ -88,13 +134,24 @@ const MeetingListComponent = ({data}) => {
 
                             <td>{list.reservationSn}</td>
                             <td>{list.meetingRoom}</td>
-                            <td>{list.useDay}</td>
+                            <td>{dayjs(list.useDay).format("YYYY년 MM월 DD일(ddd)")}</td>
                             <td>{(dayjs(list.useStartTime).format('HH시mm분 A'))}</td>
                             <td>{(dayjs(list.useEndTime).format('HH시mm분 A'))}</td>
-                            <td>{(dayjs(list.frstRegistDt).format('YYYY년MM월DD일 HH시mm분 A'))}</td>
-                            <td>{(dayjs(list.lastChangeDt).format('YYYY년MM월DD일 HH시mm분 A'))}</td>
+                            <td>{(dayjs(list.frstRegistDt).format('YYYY년 MM월 DD일 ddd HH시mm분 A'))}</td>
+                            {
+                                list.frstRegistDt === list.lastChangeDt
+                                ?
+                                    <td>수정하지 않았음</td>
+                                    :
+                                    <td>{(dayjs(list.lastChangeDt).format('YYYY년MM월DD일 HH시mm분 A'))}</td>
+                            }
                             <td>{list.memberSn.membNm}</td>
                             <td>{list.purpose}</td>
+                            {
+                                list.memberSn.membId === id
+                                ? <td><Button onClick={() => {reservationUpdate(list.reservationSn)}} value="수정"/></td>
+                                : <td>x</td>
+                            }
                             {/*<td><Button onClick={() => {noticeUpdate(list.noticeSn)}} value="수정"/></td>*/}
                         </tr>
                     );
