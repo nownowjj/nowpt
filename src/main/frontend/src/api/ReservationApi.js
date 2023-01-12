a3e7dab7d7f318f43fa3537ddd717d1e753fc6a9
jiwon.kim <jiwon.kim01@e4net.net>
2023-01-12 14:38:49 +0900

    예약 리스트 API

--- src/main/frontend/src/api/ReservationApi.js
+++ src/main/frontend/src/api/ReservationApi.js
@@ -1,4 +1,4 @@
-import {API_BASE, COMMON, NOTICE, request, RESERVATION} from "./Api";
+import {API_BASE, COMMON, request, RESERVATION} from "./Api";
 
 export function insertReservation(reservationDto){
     return request({
@@ -8,9 +8,20 @@ export function insertReservation(reservationDto){
     })
 }
 
-export function getReservation(){
+export function getReservation(paramStart,paramEnd,room){
     return request({
-        url: API_BASE + COMMON +RESERVATION + "/selectPagingReservation",
+        url: API_BASE + COMMON +RESERVATION +
+            "/selectPagingReservation?startDay=" + paramStart +
+            "&endDay=" + paramEnd +
+            "&room=" + room ,
         method: 'GET'
     })
+}
+
+export function selectReservationByReservationSn(reservationSn) {
+    return request({
+        url: API_BASE +COMMON+ RESERVATION +"/"+ reservationSn,
+        method: 'GET'
+        // body: JSON.stringify(noticeDto)
+    });
 }
