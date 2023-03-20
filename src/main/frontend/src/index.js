57116a47d49a58979868363ec7d8bbafef40f4b2
jiwon.kim <jiwon.kim01@e4net.net>
2023-03-20 15:01:33 +0900

    OJDBC 변경

--- src/main/frontend/src/index.js
+++ src/main/frontend/src/index.js
@@ -4,14 +4,21 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import 'bootstrap/dist/css/bootstrap.css';
+import {Provider} from "react-redux";
 
 // const root = ReactDOM.createRoot(document.getElementById('root'));
 
+const createStoreWidthMiddleware = applyMiddleware(
+    promiseMiddlerware,
+    reduxThunk
+)(createStoreHook);
 
 ReactDOM.render(
     // <React.StrictMode> 태그로 <app/>이 감싸져있으면
     // 개발모드에서 (개발 단계시 오류를 잘 잡기위해) 두 번씩 렌더링됩니다.
+    <Provider store={}>
         <App />
+    </Provider>
 ,document.getElementById('root')
 );
 
