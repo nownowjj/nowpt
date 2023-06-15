d384f9979a1a29d054d264c0810d1db6578f1969
jiwon.kim <jiwon.kim01@e4net.net>
2023-06-15 11:18:15 +0900

    [kakaoMap] key err 수정

--- src/main/frontend/src/services/kakaomap/Kakaomap.js
+++ src/main/frontend/src/services/kakaomap/Kakaomap.js
@@ -8,8 +8,9 @@ const KakaoMap = () => {
     console.log("맵!")
     return (
         <Map center={{ lat: 37.365264512305174, lng: 127.10676860117488 }} style={{ width: "600px", height: "600px" }} level={10}>
-            {markerdata.map((position,index : position.index) =>
-                <>
+            {/*{markerdata.map((position,index : position.index) =>*/}
+            {markerdata.map((position,index) =>
+                <div  key ={index}>
                     <MapMarker
                         key={index}
                         position={{ lat: position.lat, lng: position.lng }}
@@ -22,7 +23,7 @@ const KakaoMap = () => {
                         }}
                         title={position.title} // 마커의 타이틀, 마커에 마우스를 올리면 타이틀이 표시됩니다
                     />
-                </>
+                </div>
             )}
         </Map>
     );
