df3fffd3c9d3bed301201529f017790caf42f5d0
jiwon.kim <jiwon.kim01@e4net.net>
2023-07-31 09:02:17 +0900

    배너 react-swiper > slick으로 변경

--- src/main/frontend/src/pages/calendar/Banner/CalendarHeaderBannerComponent.js
+++ src/main/frontend/src/pages/calendar/Banner/CalendarHeaderBannerComponent.js
@@ -1,43 +1,37 @@
 import React from 'react';
-import styled from "styled-components";
-import { Swiper, SwiperSlide } from 'swiper/react';
-import 'swiper/css';
-import 'swiper/css/navigation';
-import 'swiper/css/pagination';
+import styled from 'styled-components';
+import Slider from 'react-slick';
+import 'slick-carousel/slick/slick.css';
+import 'slick-carousel/slick/slick-theme.css';
 
 const CalendarHeaderBannerComponent = () => {
-
     return (
         <CalendarHeaderBannerWrap>
-            <Swiper
-                spaceBetween={10}
-                slidesPerView={1}
-                navigation
-                loop={true}
-                autoplay={{ delay: 100 }}
-                effect={'fade'}
-                pagination={{
-                    clickable: true,
-                }}
+            <Slider
+                dots={false}
+                infinite={true}
+                speed={1000} // Time in milliseconds between each slide change
+                slidesToShow={1}
+                slidesToScroll={1}
+                autoplay={true}
+                autoplaySpeed={5000} // Time in milliseconds to wait before moving to the next slide
+                fade={true}
             >
-                <SwiperSlide>
-                    <div>캘린더 앱</div>
-                </SwiperSlide>
-                <SwiperSlide>
-                    <div>열심히 기록합시다</div>
-                </SwiperSlide>
-                <SwiperSlide>
-                    <div>가보자구~</div>
-                </SwiperSlide>
-            </Swiper>
+                <BannerItem>NowNowjj</BannerItem>
+                <BannerItem>Calendar App</BannerItem>
+                <BannerItem>My Record</BannerItem>
+            </Slider>
         </CalendarHeaderBannerWrap>
     );
 };
-
-const CalendarHeaderBannerWrap = styled.div`
-    padding: 0 6px; 
-    font-size: 14px;
+const BannerItem = styled.div`
+    padding:0px 2px;
+    font-size:14px;
+    font-weight:bold;
 `
-
+const CalendarHeaderBannerWrap = styled.div`
+  padding: 0 6px;
+  font-size: 14px;
+`;
 
 export default CalendarHeaderBannerComponent;
